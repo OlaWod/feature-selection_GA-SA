5567058bfe8ca28b499a816a646480c025d8485f
OlaWod <756825440@qq.com>
2020-04-04 13:44:47 +0800

    first commit

---
+++ GA_feature_selection.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from sklearn import tree
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_extraction.text import CountVectorizer
+'''
+train_data = pd.read_csv('train.tsv', sep='\t')  # 训练集
+test_data = pd.read_csv('test.tsv', sep='\t')   # 测试集
+
+corpus_train = train_data['Phrase']   # 语料库
+corpus_test = test_data['Phrase']   # 语料库
+
+vectorizer = CountVectorizer(ngram_range=(1,3), max_features = 150000) # N元特征
+vectorizer.fit(pd.concat([corpus_train,corpus_test]))
+
+X = vectorizer.transform(corpus_train)    # 向量化
+#X_test = vectorizer.transform(corpus_test)    # 向量化
+
+y = list(train_data['Sentiment'])
+'''
+data = pd.read_csv('./dataset/sonar.all-data',header=None,sep=',')
+print(data.head())
+X = data.iloc[:,:-1]
+y = data.iloc[:,-1:].values.flatten()
+
+iterations = 100 # 迭代次数
+pop_size = 100   # 种群大小，多少个染色体
+pc = 0.25   # 交叉概率
+pm = 0.01   # 变异概率
+
+chrom_length = len(data.columns)-1    # 染色体长度
+pop = []    # 种群
+fitness_list = []   # 适应度
+ratio_list = []     # 累计概率
+
+
+# 初始化种群
+def geneEncoding():
+    i = 0
+    while i < pop_size:
+        temp = []
+        has_1 = False   # 这条染色体是否有1
+        for j in range(chrom_length):
+            rand = random.randint(0,1)
+            if rand == 1:
+                has_1 = True
+            temp.append(rand)
+        if has_1:   # 染色体不能全0
+            i += 1
+            pop.append(temp)
+        
+
+# 计算适应度
+def calFitness():
+    fitness_list.clear()
+    for i in range(pop_size):   # 计算种群中每条染色体的适应度
+        X_test = X
+
+        has_1 = False
+        for j in range(chrom_length):
+            if pop[i][j] == 0:
+                X_test =X_test.drop(columns = j)
+            else:
+                has_1 = True
+        X_test = X_test.values
+        
+        if has_1:
+            clf = tree.DecisionTreeClassifier() # 决策树作为分类器
+            fitness = cross_val_score(clf, X_test, y, cv=5).mean()  # 5次交叉验证
+            fitness_list.append(fitness)
+        else:
+            fitness = 0     # 全0的适应度为0
+            fitness_list.append(fitness)
+
+# 计算适应度的总和
+def sumFitness():
+    total = 0
+    for i in range(pop_size):
+        total += fitness_list[i]
+    return total
+
+# 计算每条染色体的累计概率
+def getRatio():
+    ratio_list.clear()
+    ratio_list.append(fitness_list[0])
+    for i in range(1, pop_size):
+        ratio_list.append(ratio_list[i-1] + fitness_list[i])
+    ratio_list[-1] = 1
+
+# 选择
+def selection():
+    global pop
+    total_fitness = sumFitness()
+    for i in range(pop_size):
+        fitness_list[i] = fitness_list[i] / total_fitness
+    getRatio()
+    
+    rand_ratio = [] # 随机概率
+    for i in range(pop_size):
+        rand_ratio.append(random.random())
+    rand_ratio.sort()
+
+    new_pop = []    # 新种群
+    i = 0  # 已经处理的随机概率数
+    j = 0  # 超出范围的染色体数
+   
+    while i < pop_size:
+        if rand_ratio[i] < ratio_list[j]:   # 随机数在第j个染色体的概率范围内
+            new_pop.append(pop[j])
+            i += 1
+        else:
+            j += 1
+
+    pop = new_pop
+
+# 交叉
+def crossover():
+    for i in range(pop_size-1): # 若交叉，则染色体i与染色体i+1交叉
+        if random.random() < pc:# 发生交叉
+            cpoint = random.randint(0, chrom_length-1)    # 随机选择交叉点
+            temp1 = []
+            temp2 = []
+            temp1.extend(pop[i][:cpoint])
+            temp1.extend(pop[i+1][cpoint:])
+            temp2.extend(pop[i+1][:cpoint])
+            temp2.extend(pop[i][cpoint:])
+            pop[i] = temp1
+            pop[i+1] = temp2
+
+# 变异
+def mutation():
+    for i in range(pop_size):
+        if random.random() < pm: # 发生变异
+            mpoint = random.randint(0, chrom_length-1)  # 随机选择变异点
+            if pop[i][mpoint] == 1:
+                pop[i][mpoint] = 0
+            else:
+                pop[i][mpoint] = 1
+
+# 最优解
+def getBest():
+    best_chrom = pop[0]
+    best_fitness = fitness_list[0]
+    for i in range(1,pop_size):
+        if fitness_list[i] > best_fitness:
+            best_fitness = fitness_list[i]  # 最佳适应值
+            best_chrom = pop[i] # 最佳染色体
+
+    return best_chrom, best_fitness
+
+if __name__=='__main__':
+    
+    plt.xlabel('iterations')
+    plt.ylabel('best fitness')
+    plt.xlim((0,iterations))    # x坐标范围
+    plt.ylim((0,1)) # y坐标范围
+    px = []
+    py = []
+    plt.ion()
+    
+    results = []
+    geneEncoding() # 初始化种群
+    for i in range(iterations):
+        print(i)
+        
+        calFitness() # 计算种群中每条染色体适应度
+        
+        best_chrom, best_fitness = getBest()
+        results.append([i, best_chrom, best_fitness])
+        
+        selection() # 选择
+        crossover() # 交叉
+        mutation()  # 变异
+
+        print([i, best_chrom, best_fitness])
+        
+        px.append(i)
+        py.append(best_fitness)
+        plt.plot(px,py)
+        plt.show()
+        plt.pause(0.001)
+        
+
---
+++ SA_feature_selection.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import math
+from sklearn import tree
+from sklearn.model_selection import cross_val_score
+
+data = pd.read_csv('./dataset/sonar.all-data',header=None,sep=',')
+print(data.head())
+X = data.iloc[:,:-1]
+y = data.iloc[:,-1:].values.flatten()
+chrom_length = len(data.columns)-1    # 染色体长度
+
+#############################################
+# 影响迭代次数
+tmp = math.exp(3)   # 初始温度
+tmp_min = math.exp(-8)    # 停止温度
+alpha = 0.98    # 降温系数
+
+counter = 10000     # 生成更差解的次数
+
+# 影响较差解采纳概率,越大越容易采纳更差解
+k = 0.002
+#############################################
+
+# 是否可采纳
+def is_acceptable(delta_E,tmp):     
+    if delta_E<0:   # ΔE<0，直接采纳
+        return True
+
+    p=math.exp(-delta_E/(k*tmp))    # 求采纳概率
+    if random.random()<p:
+        return True
+    else:
+        return False
+
+# 特征编码
+def geneEncoding():
+    while True:
+        temp = []
+        has_1 = False   # 这条染色体是否有1
+        for j in range(chrom_length):
+            rand = random.randint(0,1)
+            if rand == 1:
+                has_1 = True
+            temp.append(rand)
+        if has_1:   # 染色体不能全0
+            return temp
+
+# 获取适应度
+def getFitness(x):
+    X_test = X
+
+    has_1 = False
+    for j in range(chrom_length):
+        if x[j] == 0:
+            X_test =X_test.drop(columns = j)
+        else:
+            has_1 = True
+    X_test = X_test.values
+        
+    if has_1:
+        clf = tree.DecisionTreeClassifier() # 决策树作为分类器
+        fitness = cross_val_score(clf, X_test, y, cv=5).mean()  # 5次交叉验证
+        return fitness
+    else:
+        fitness = 0     # 全0的适应度为0
+        return fitness
+
+# 从旧解生成新解
+def getNewChrom(x):
+    mpoint = random.randint(0, chrom_length-1)  # 随机选择变异点
+    if x[mpoint] == 1:
+        x[mpoint] = 0
+    else:
+        x[mpoint] = 1
+    return x
+
+# 程序入口
+if __name__=='__main__':
+    plt.xlabel('temperature')
+    plt.ylabel('fitness')
+    plt.xlim((tmp_min,tmp))    # x坐标范围
+    plt.ylim((0.4,0.9)) # y坐标范围
+    px = []
+    py_old = []
+    py_new = []
+    plt.ion()
+    
+    x_old = geneEncoding()    # 生成初始随机解
+    E_old = getFitness(x_old)
+
+    while tmp > tmp_min:
+
+        x_new = getNewChrom(x_old)   # 生成随机解
+        E_new = getFitness(x_new)
+        delta_E = -(E_new - E_old)
+
+        if is_acceptable(delta_E,tmp):  # 可采纳
+            x_old = x_new
+            E_old = E_new
+
+        if delta_E<0:   # ΔE<0，降温
+            tmp = tmp * alpha
+        else:
+            counter += 1
+
+        if counter < 0:
+            break
+
+        print(x_old)
+        print(E_old)
+
+        px.append(tmp)
+        py_old.append(E_old)
+        py_new.append(E_new)
+        plt.plot(px,py_old,'r')
+        plt.plot(px,py_new)
+        plt.show()
+        plt.pause(0.001)
+    
